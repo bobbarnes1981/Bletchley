cbc29ca00eb2996965c5ca3a22adda8077598572
Robert Barnes <robertbarnes1981@googlemail.com>
2016-09-21 06:59:24 +0100

    Implement plugboard

---
+++ Bletchley.Enigma.Simulator.UnitTests/PlugboardTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Bletchley.Enigma.Simulator.UnitTests
+{
+    class PlugboardTests
+    {
+        [Test]
+        public void CheckPlugboardFailsWithMatchigKeyValue()
+        {
+            Exception exception = Assert.Throws<Exception>(() => new Plugboard(new Dictionary<Letters, Letters> { { Letters.A, Letters.B }, { Letters.C, Letters.A } }));
+
+            Assert.That(exception.Message, Is.EqualTo("matching key and value A"));
+
+            exception = Assert.Throws<Exception>(() => new Plugboard(new Dictionary<Letters, Letters> { { Letters.B, Letters.A }, { Letters.A, Letters.C } }));
+
+            Assert.That(exception.Message, Is.EqualTo("matching key and value A"));
+        }
+
+        [Test]
+        public void CheckPlugboardFailsWithDuplicateMap()
+        {
+            Exception exception = Assert.Throws<Exception>(() => new Plugboard(new Dictionary<Letters, Letters> { { Letters.A, Letters.B }, { Letters.C, Letters.B } }));
+
+            Assert.That(exception.Message, Is.EqualTo("duplicate mapping to B"));
+        }
+
+        [Test]
+        public void CheckPlugboardFailsWithDirectMap()
+        {
+            Exception exception = Assert.Throws<Exception>(() => new Plugboard(new Dictionary<Letters, Letters> { { Letters.A, Letters.A } }));
+
+            Assert.That(exception.Message, Is.EqualTo("direct mapping for A"));
+        }
+
+        [Test]
+        public void CheckPlugboardSucceeds()
+        {
+            Plugboard p = new Plugboard(new Dictionary<Letters, Letters> { { Letters.A, Letters.B } });
+        }
+
+        [Test]
+        public void CheckPlugboardReturnsCorrectForMapped()
+        {
+            Plugboard p = new Plugboard(new Dictionary<Letters, Letters> { { Letters.A, Letters.B } });
+
+            Letters result;
+
+            result = p.GetOutput(Letters.A);
+
+            Assert.That(result, Is.EqualTo(Letters.B));
+
+            result = p.GetOutput(Letters.B);
+
+            Assert.That(result, Is.EqualTo(Letters.A));
+        }
+
+        [Test]
+        public void CheckPlugboardReturnsCorrectForUnmapped()
+        {
+            Plugboard p = new Plugboard(new Dictionary<Letters, Letters> { { Letters.A, Letters.B } });
+
+            Letters result = p.GetOutput(Letters.C);
+
+            Assert.That(result, Is.EqualTo(Letters.C));
+        }
+    }
+}
--- Bletchley.Enigma.Simulator/Plugboard.cs
+++ Bletchley.Enigma.Simulator/Plugboard.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bletchley.Enigma.Simulator
 {
-    class Plugboard
+    public class Plugboard
     {
+        private Dictionary<Letters, Letters> m_map;
+
+        public Plugboard(Dictionary<Letters, Letters> map)
+        {
+            foreach (KeyValuePair<Letters, Letters> pair in map)
+            {
+                if (pair.Key == pair.Value)
+                {
+                    throw new Exception(string.Format("direct mapping for {0}", pair.Key));
+                }
+
+                if (map.Values.Count(v => v == pair.Value) > 1)
+                {
+                    throw new Exception(string.Format("duplicate mapping to {0}", pair.Value));
+                }
+
+                if (map.Values.Count(v => v == pair.Key) > 0)
+                {
+                    throw new Exception(string.Format("matching key and value {0}", pair.Key));
+                }
+            }
+
+            m_map = map;
+        }
+
+        public Letters GetOutput(Letters input)
+        {
+            if (m_map.ContainsKey(input))
+            {
+                return m_map[input];
+            }
+
+            if (m_map.ContainsValue(input))
+            {
+                return m_map.First(p => p.Value == input).Key;
+            }
+
+            return input;
+        }
     }
 }
